5cb1a7a6d2c23597a9fae920cb278f6597765c32
Saito Nakamura <saitonakamuramb@gmail.com>
2018-11-09 13:29:01 +0100

    :sparkles: Added support for loading resoursec dynamically

--- index.tsx
+++ index.tsx
@@ -12,14 +12,19 @@ const LangContext = React.createContext({
   l: (key: any) => key.toString() as React.ReactNode,
   lang: 'en' as any,
   onLangChange: (_: any) => {},
+  isLoadingLang: false,
 })
+
+interface FetchResourceFunc<TResource> {
+  (): Promise<TResource>
+}
 
 export interface LangProviderProps<
   TLangs extends keyof any,
   TResource extends object
 > {
-  lang: TLangs
-  resources: Record<TLangs, TResource>
+  initialLang: TLangs
+  initialResources: Record<TLangs, TResource | FetchResourceFunc<TResource>>
 }
 
 interface LangProviderState<
@@ -29,6 +34,7 @@ interface LangProviderState<
   lang: TLangs
   onLangChange: (newLang: TLangs) => void
   l: LocalizeFunc<TResource>
+  isLoadingLang: boolean
 }
 
 export class LangProvider<
@@ -38,12 +44,26 @@ export class LangProvider<
   LangProviderProps<TLangs, TResource>,
   LangProviderState<TLangs, TResource>
 > {
+  private resources: LangProviderProps<TLangs, TResource>['initialResources']
+
   constructor(props: LangProviderProps<TLangs, TResource>) {
     super(props)
+
+    this.resources = props.initialResources
+
     this.state = {
-      lang: props.lang,
+      lang: props.initialLang,
       onLangChange: this.handleLangChange,
       l: this.localize,
+      isLoadingLang: false,
+    }
+
+    const selectedResouce = this.resources[props.initialLang]
+    if (this.isFetchFunc(selectedResouce)) {
+      ;(this.state as any).isLoadingLang = true
+      this.loadResource(selectedResouce, props.initialLang).then(() =>
+        this.setState({ isLoadingLang: false }),
+      )
     }
   }
 
@@ -53,12 +73,49 @@ export class LangProvider<
     return typeof key === 'string'
   }
 
-  handleLangChange = (newLang: TLangs) => this.setState({ lang: newLang })
+  isObject(
+    resource: TResource | FetchResourceFunc<TResource>,
+  ): resource is TResource {
+    return typeof resource === 'object'
+  }
+
+  isFetchFunc(
+    resource: TResource | FetchResourceFunc<TResource>,
+  ): resource is FetchResourceFunc<TResource> {
+    return resource instanceof Function
+  }
+
+  handleLangChange = (newLang: TLangs) => {
+    const { lang } = this.state
+    const resource = this.resources[lang]
+
+    if (this.isFetchFunc(resource)) {
+      this.loadResource(resource, lang).then(() =>
+        this.setState({ lang: newLang }),
+      )
+    } else {
+      this.setState({ lang: newLang })
+    }
+  }
+
+  loadResource = (resource: FetchResourceFunc<TResource>, lang: TLangs) =>
+    resource().then(res => {
+      this.resources = { ...(this.resources as any), [lang]: res }
+    })
 
   localize: LocalizeFunc<TResource> = key => {
-    const resource = this.props.resources[this.state.lang]
-    if (this.isKeyof(key)) return resource[key]
-    else return key(resource)
+    const { lang } = this.state
+    const resource = this.resources[lang]
+
+    if (this.isObject(resource)) {
+      if (this.isKeyof(key)) return resource[key]
+      else return key(resource)
+    }
+
+    if (this.isFetchFunc(resource))
+      throw Error('resource should be loaded at this moment')
+
+    throw Error('resource is neither object nor promise create function')
   }
 
   render() {
