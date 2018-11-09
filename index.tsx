7efe16ccf1a263fa990e134bfa21645e98fe0c44
Saito Nakamura <saitonakamuramb@gmail.com>
2018-11-09 12:24:14 +0100

    :art: Set up prettier as formatter

--- index.tsx
+++ index.tsx
@@ -1,55 +1,64 @@
-import * as React from "react";
+import * as React from 'react'
 
 interface ResourseProviderFunc<TResource> {
   (strings: TResource): React.ReactNode
 }
 
 interface LocalizeFunc<TResource> {
-  (key: keyof TResource | ResourseProviderFunc<TResource>): React.ReactNode;
+  (key: keyof TResource | ResourseProviderFunc<TResource>): React.ReactNode
 }
 
-const LangContext = React.createContext({ 
+const LangContext = React.createContext({
   l: (key: any) => key.toString() as React.ReactNode,
   lang: 'en' as any,
-  onLangChange: (_: any) => {}
-});
+  onLangChange: (_: any) => {},
+})
 
-export interface LangProviderProps<TLangs extends keyof any, TResource extends object> {
-  lang: TLangs;
+export interface LangProviderProps<
+  TLangs extends keyof any,
+  TResource extends object
+> {
+  lang: TLangs
   resources: Record<TLangs, TResource>
-};
+}
 
-interface LangProviderState<TLangs extends keyof any, TResource extends object> {
-  lang: TLangs;
-  onLangChange: (newLang: TLangs) => void;
+interface LangProviderState<
+  TLangs extends keyof any,
+  TResource extends object
+> {
+  lang: TLangs
+  onLangChange: (newLang: TLangs) => void
   l: LocalizeFunc<TResource>
-};
+}
 
-export class LangProvider<TLangs extends keyof any, TResource extends object>
-  extends React.Component<LangProviderProps<TLangs, TResource>, LangProviderState<TLangs, TResource>> {
-
+export class LangProvider<
+  TLangs extends keyof any,
+  TResource extends object
+> extends React.Component<
+  LangProviderProps<TLangs, TResource>,
+  LangProviderState<TLangs, TResource>
+> {
   constructor(props: LangProviderProps<TLangs, TResource>) {
-    super(props);
+    super(props)
     this.state = {
       lang: props.lang,
       onLangChange: this.handleLangChange,
-      l: this.localize
-    };
+      l: this.localize,
+    }
   }
 
-  isKeyof(key: keyof TResource | ResourseProviderFunc<TResource>): key is keyof TResource {
+  isKeyof(
+    key: keyof TResource | ResourseProviderFunc<TResource>,
+  ): key is keyof TResource {
     return typeof key === 'string'
   }
 
-  handleLangChange = (newLang: TLangs) =>
-    this.setState({ lang: newLang });
+  handleLangChange = (newLang: TLangs) => this.setState({ lang: newLang })
 
   localize: LocalizeFunc<TResource> = key => {
     const resource = this.props.resources[this.state.lang]
-    if (this.isKeyof(key))
-      return resource[key];
-    else
-      return key(resource);
+    if (this.isKeyof(key)) return resource[key]
+    else return key(resource)
   }
 
   render() {
@@ -57,16 +66,18 @@ export class LangProvider<TLangs extends keyof any, TResource extends object>
       <LangContext.Provider value={this.state}>
         {this.props.children}
       </LangContext.Provider>
-    );
+    )
   }
 }
 
-export class LangConsumer<TLangs extends keyof any, TResource extends object>
-  extends React.Component<React.ConsumerProps<LangProviderState<TLangs, TResource>>, {}> {
-
+export class LangConsumer<
+  TLangs extends keyof any,
+  TResource extends object
+> extends React.Component<
+  React.ConsumerProps<LangProviderState<TLangs, TResource>>,
+  {}
+> {
   render() {
-    return (
-      <LangContext.Consumer {...this.props} />
-    )
+    return <LangContext.Consumer {...this.props} />
   }
 }
